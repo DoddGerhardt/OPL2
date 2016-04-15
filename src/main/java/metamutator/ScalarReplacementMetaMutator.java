456ce1523a140776f3111cead82dd55bf0db577b
Étienne <etienne@wattebled.fr>
2016-04-15 21:33:53 +0200

    màj

--- src/main/java/metamutator/ScalarReplacementMetaMutator.java
+++ src/main/java/metamutator/ScalarReplacementMetaMutator.java
@@ -8,22 +8,37 @@ import java.util.Map;
 
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtVariableAccess;
+import spoon.reflect.code.CtVariableRead;
+import spoon.reflect.code.CtVariableWrite;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtVariable;
+import spoon.reflect.reference.CtParameterReference;
+import spoon.reflect.reference.CtVariableReference;
+import spoon.support.reflect.reference.CtVariableReferenceImpl;
 
-public class ScalarReplacementMetaMutator extends AbstractProcessor<CtVariable<?>> {
+public class ScalarReplacementMetaMutator extends AbstractProcessor<CtElement> {
 	public static final String PREFIX = "_scalarReplacementHotSpot";
 	private static int index = 0;
 	private Map<String,Map<String,List<CtVariable<?>>>> variables;
 	private List<CtVariable<?>> variablesCompatibles;
+	private CtVariable<?> variable;
 	
 	public ScalarReplacementMetaMutator() {
 		variables = new HashMap<String,Map<String,List<CtVariable<?>>>>();
 	}
 	@Override
-	public boolean isToBeProcessed(CtVariable<?> variable) {
+	public boolean isToBeProcessed(CtElement element) {
+		if (element instanceof CtVariable) {
+			this.variable = (CtVariable<?>) element;
+		} else if (element instanceof CtVariableAccess) {
+			this.variable = ((CtVariableAccess<?>) (element)).getVariable().getDeclaration();
+		} else {
+			return false;
+		}
+		System.out.println(variable.getSimpleName());
 		CtClass<?> c = variable.getParent(CtClass.class);
 		Map<String,List<CtVariable<?>>> map = variables.get(c.getSimpleName());
 		if (map == null) {
@@ -68,14 +83,15 @@ public class ScalarReplacementMetaMutator extends AbstractProcessor<CtVariable<?
 	}
 	
 	@Override
-	public void process(CtVariable<?> variable) {
+	public void process(CtElement element) {
+		/**
 		System.out.print(variable.getSimpleName());
 		System.out.print(" : ");
-		for (CtVariable<?> v : variablesCompatibles) {
-			System.out.print(v.getSimpleName());
+		 for (CtVariable<?> v : variablesCompatibles) {
+		 	System.out.print(v.getSimpleName());
 			System.out.print(" ");
-		}
-		System.out.println("");
-		
+		 }
+		 System.out.println("");
+		**/
 	}
 }
