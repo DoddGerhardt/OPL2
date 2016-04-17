e640b5be1c6235f24a6e96aa557795f0a6f42d80
Étienne <etienne@wattebled.fr>
2016-04-17 19:21:59 +0200

    màj

---
+++ src/main/java/FileExtractor.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+
+public class FileExtractor {
+	
+	private File path;
+	private String extension;
+	
+	public FileExtractor(File path, String extension) {
+		this.path = path;
+		this.extension = extension;
+	}
+	public FileExtractor(String path, String extension) {
+		this(new File(path),extension);
+	}
+
+	public List<File> extract() {
+		List<File> list = new LinkedList<File>();
+		extract(path,list);
+		return list;
+	}
+	
+	private void extract(File currentFile, List<File> files) {
+		if (currentFile != null) {
+			if (currentFile.isFile()) {
+				if (currentFile.toString().endsWith(extension)) {
+					files.add(currentFile);
+				}
+			} else {
+				for (File f : currentFile.listFiles()) {
+					extract(f,files);
+				}
+			}
+		}
+	}
+	
+	public static List<File> extract(File path, String extension) {
+		return new FileExtractor(path,extension).extract();
+	}
+	public static List<File> extract(String path, String extension) {
+		return new FileExtractor(path,extension).extract();
+	}
+}
--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -1,8 +1,20 @@
+import java.io.File;
+
+import spoon.Launcher;
+import spoon.compiler.SpoonCompiler;
 import metamutator.MutantSearchSpaceExplorator;
 
 
 public class Launch {
 	public static void main(String args[]) throws Exception {
-		spoon.Launcher.main(new String[] {"-i","input/jsoup/src/main/java","-o","spooned","-p","metamutator.UnaryOperatorMetaMutator"});
+		spoon.Launcher.main(new String[] {"-i","input/jsoup/src/main/java","-o","spooned/jsoup/src/main/java","-p","metamutator.UnaryOperatorMetaMutator"});
+		
+		Launcher spoon = new Launcher();
+		SpoonCompiler comp = spoon.createCompiler();
+		comp.addInputSource(new File("input"));
+		comp.setBinaryOutputDirectory(new File("spooned"));
+		comp.compile();
+		
+		MutantSearchSpaceExplorator.runMetaProgramIn("input/jsoup/target/test-classes","resources/search_replay_test");
 	}
 }
