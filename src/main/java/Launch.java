5f2084d80c407d65f2a9da94468ac666c739d954
LorneMalvoo <delaeter.jb@gmail.com>
2016-04-17 19:00:54 +0200

    maj launcher

--- src/main/java/Launch.java
+++ src/main/java/Launch.java
@@ -3,6 +3,6 @@ import metamutator.MutantSearchSpaceExplorator;
 
 public class Launch {
 	public static void main(String args[]) throws Exception {
-		spoon.Launcher.main(new String[] {"-i","input/jsoup/src/main/java","-o","spooned/jsoup/src/main/java","-p","metamutator.UnaryOperatorMetaMutator"});
+		spoon.Launcher.main(new String[] {"-i","input/jsoup/src/main/java","-o","spooned","-p","metamutator.UnaryOperatorMetaMutator"});
 	}
 }
